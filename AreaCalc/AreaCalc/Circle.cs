1fdd52627c5a3a65361b5c0f8dcb15005bb233a1
Outrageous258 <koosthuizen@retrorabbit.co.za>
2020-01-17 10:32:29 +0200

    The circle class and an updated circle reference to the main method

---
+++ AreaCalc/AreaCalc/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AreaCalc
+{
+    public class Circle
+    {
+        int width;
+        public Circle(int width)
+        {
+            this.width = width;
+        }
+        public Decimal area()
+        {
+            Double.Parse(width + "");
+            double area = Math.PI * (Math.Pow((Double.Parse(width + "")), 2.0));
+            return Decimal.Parse(area + "");
+        }
+    }
+}
