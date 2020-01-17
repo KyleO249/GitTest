09c8acb2ecef179f04ff6b705e9e35f83828c656
Outrageous258 <koosthuizen@retrorabbit.co.za>
2020-01-17 09:54:40 +0200

    Edited Main

--- AreaCalc/AreaCalc/Program.cs
+++ AreaCalc/AreaCalc/Program.cs
@@ -6,7 +6,28 @@ namespace AreaCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, welcome to the area calculator");
+            Console.Write("Please select your shape (1 = Cicrle, 2 = Square, 3 = Triangle, 4 = Rectangle): ");
+            int option = int.Parse(Console.ReadLine());
+            int width;
+            if (option == 1)//This is the circle
+            {
+                
+                Console.Write("Please enter in the width of your shape: ");
+                width = int.Parse(Console.ReadLine());
+            }
+            else if(option == 2 || option == 3 || option == 4)//This is any other object
+            {
+                Console.Write("Please enter in the width of your shape: ");
+                width = int.Parse(Console.ReadLine());
+                Console.Write("Please enter in the height of your shape: ");
+                int height = int.Parse(Console.ReadLine());
+            }
+            else//This is an error
+            {
+                Console.WriteLine("That was not and option");
+            }
+            
         }
     }
 }
