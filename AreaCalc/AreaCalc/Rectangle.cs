b86a18f571dbbf42af386a23e6bef200c831b176
Pmotsumi <Pmotsumi@retrorabbit.co.za>
2020-01-17 10:51:52 +0200

    rect added

---
+++ AreaCalc/AreaCalc/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaCalc
+{
+    class Reactangle
+    {
+        // Perimeters of a rectangle 
+        public decimal length, breadth;
+
+        // Method for calculating area of rectangle
+        public decimal reactangleArea()
+        {
+            Console.WriteLine("Length of rectangle");
+            length = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Breadth of rectangle");
+            breadth = decimal.Parse(Console.ReadLine());
+            return length * breadth;
+        }
+    }
+}
