b395f8c8e34752fd395f274a1f94d280401d485a
Outrageous258 <koosthuizen@retrorabbit.co.za>
2020-01-17 10:08:56 +0200

    Removed the menu options and added the calls to each respective class and area method

--- AreaCalc/AreaCalc/Program.cs
+++ AreaCalc/AreaCalc/Program.cs
@@ -7,27 +7,24 @@ namespace AreaCalc
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, welcome to the area calculator");
-            Console.Write("Please select your shape (1 = Cicrle, 2 = Square, 3 = Triangle, 4 = Rectangle): ");
-            int option = int.Parse(Console.ReadLine());
-            int width;
-            if (option == 1)//This is the circle
-            {
-                
-                Console.Write("Please enter in the width of your shape: ");
-                width = int.Parse(Console.ReadLine());
-            }
-            else if(option == 2 || option == 3 || option == 4)//This is any other object
-            {
-                Console.Write("Please enter in the width of your shape: ");
-                width = int.Parse(Console.ReadLine());
-                Console.Write("Please enter in the height of your shape: ");
-                int height = int.Parse(Console.ReadLine());
-            }
-            else//This is an error
-            {
-                Console.WriteLine("That was not and option");
-            }
-            
+            Console.Write("Please select your width for the cicle: ");
+            int width = int.Parse(Console.ReadLine());
+            Circle circle = new Circle(width);
+            Console.WriteLine("The area of your cicle is: " + circle.Area());
+            Console.Write("Please select your width for the triangle: ");
+            width = int.Parse(Console.ReadLine());
+            Triangle triangle = new Triangle(width);
+            Console.WriteLine("The area of your triangle is: " + triangle.Area());
+            Console.Write("Please select your width for the square: ");
+            width = int.Parse(Console.ReadLine())
+            Square square = new Square(width);
+            Console.WriteLine("The area of your square is: " + square.Area());
+            Console.Write("Please select your width for the rectangle: ");
+            width = int.Parse(Console.ReadLine());
+            Console.Write("Please enter in the height of your rectangle: ");
+            int height = int.Parse(Console.ReadLine())
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("The area of your square is: " + rectangle.Area());
         }
     }
 }
