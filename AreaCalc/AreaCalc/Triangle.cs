e9c89d7ab3fc4f5c0f62fdef10099db5590b415b
kbester <kbester@retrorabbit.co.za>
2020-01-17 10:43:56 +0200

    Adding Triangle class file

---
+++ AreaCalc/AreaCalc/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Triangle
+{
+    decimal Length;
+    public Triangle(decimal Side)
+    {
+        this.Length = Side;
+    }
+
+    public decimal Area()
+    {
+        decimal Area = (Length * Length) / 4;
+        return Area;
+    }
+}
