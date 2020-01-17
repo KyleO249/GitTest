952f7fba6bea9a81e890f689a0657d71391ac817
arsalan366 <ashahid@retrorabbit.co.za>
2020-01-17 11:37:45 +0200

    removed spaces

--- AreaCalc/AreaCalc/Square.cs
+++ AreaCalc/AreaCalc/Square.cs
@@ -4,11 +4,9 @@ namespace Git_test
 {
     class Square
     {
-      
         public double calculateSquareArea(double length)
         {
             return length * length;
         }
-        
     }
 }
