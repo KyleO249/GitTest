a477fb4e3563f95b28933712e6b69ed33d366d23
arsalan366 <ashahid@retrorabbit.co.za>
2020-01-17 10:36:31 +0200

    this is square

---
+++ AreaCalc/AreaCalc/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Git_test
+{
+    class Square
+    {
+      
+        public void calculateSquareArea()
+        {
+            Console.Write("a = ");
+            int length = int.Parse(Console.ReadLine());
+            int area = length * length;
+            Console.Write("Square area = " + area);
+            
+
+        }
+        
+    }
+}
